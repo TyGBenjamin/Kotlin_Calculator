472af4b7e001588e50b79cff01d71a05f757880f
Tyler Gandy <tylergandy@Raves-MacBook-Air.local>
2022-09-21 00:45:06 -0400

    Changes made to layout of Calculator

--- app/src/main/java/com/rave/calculator/MainActivity.kt
+++ app/src/main/java/com/rave/calculator/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.rave.calculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+interface MyInterFace {
+    fun doStuff()
+}
+
+
+class MainActivity : AppCompatActivity(), MyInterFace {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val startButton = findViewById<Button>(R.id.btn_start_button)
+
+        startButton.setOnClickListener {
+            Log.d(TAG, "onCreate: start button is working ")
+            val calculatorIntent = Intent(applicationContext, CalculatorActivity::class.java)
+            startActivity(calculatorIntent)
+        }
+
+
+    }
+    companion object{
+        const val TAG="MainActivityLog"
+    }
+
+    override fun doStuff() {
+        TODO("Not yet implemented")
     }
 }
