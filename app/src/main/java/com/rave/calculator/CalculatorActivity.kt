137cf288ae1656e3a4e9ddf15eb085e3217eb4a9
Tyler Gandy <tylergandy@Raves-MacBook-Air.local>
2022-09-21 12:02:45 -0400

    changes made to init views

--- app/src/main/java/com/rave/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/rave/calculator/CalculatorActivity.kt
@@ -7,28 +7,29 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.rave.calculator.CalculatorActivity.Companion.TAG
+
 //import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : AppCompatActivity() {
 
-var inputDisplay: TextView = findViewById<TextView>(R.id.calc_activity_text)
-var outputDisplay: TextView = findViewById<TextView>(R.id.result_text)
-private var canAdd = false
-
+    private lateinit var inputDisplay: TextView
+    private lateinit var outputDisplay: TextView
+    private var canAdd = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+        initViews()
         val leftParenButton = findViewById<Button>(R.id.btn_leftParenthesis)
         val rightParenButton = findViewById<Button>(R.id.btn_rightParenthesis)
-        val equalsButton= findViewById<Button>(R.id.btn_equals)
+        val equalsButton = findViewById<Button>(R.id.btn_equals)
         val clearButton = findViewById<Button>(R.id.btn_clear)
-        
 
 
 
-        clearButton.setOnClickListener{
+
+        clearButton.setOnClickListener {
             Log.d(TAG, "onCreate: clear button clicked")
             inputDisplay.text = " "
             outputDisplay.text = ""
@@ -37,6 +38,10 @@ private var canAdd = false
 
     }
 
+    fun initViews() {
+        inputDisplay = findViewById(R.id.calc_activity_text)
+        outputDisplay = findViewById(R.id.result_text)
+    }
 //    fun numberInput(view: View){
 //        if(view is Button){
 //            inputDisplay.append(view.text)
@@ -55,8 +60,8 @@ private var canAdd = false
 //    }
 //    fun equalsInput(view: View){}
 
-    companion object{
-        const val TAG="CalcActivityLog"
+    companion object {
+        const val TAG = "CalcActivityLog"
     }
 }
 
